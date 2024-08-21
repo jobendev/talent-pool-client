e1059ec6284bd985ad75c79c8684f1aba041abac
Joben <jjvillodrez@gmail.com>
2024-08-22 05:02:39 +0800

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+      <h1 className="text-3xl font-bold underline">Test</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
