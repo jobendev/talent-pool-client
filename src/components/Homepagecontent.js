c185063e369a9a3b68056578ed2108fca43b4ac3
Joben <jjvillodrez@gmail.com>
2024-08-24 01:53:15 +0800

    add new js file for homepage content

---
+++ src/components/Homepagecontent.js
@@ -0,0 +1,20 @@
+const homePageContents = [
+    {
+    id: 1,
+    content: "lorem ipsum",
+    image: "./"
+    },
+    {
+        id: 2,
+        content: "lorem ipsum",
+        image: "./"
+    },
+    {
+        id: 3,
+        content: "lorem ipsum",
+        image: "./"
+    },
+]
+
+
+export default homePageContents;
